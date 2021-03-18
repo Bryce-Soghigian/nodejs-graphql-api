14c087f4ad49b61a2cc62419692b0093d9cfbaa0
Bryce-Soghigian <brybrycodingguy@gmail.com>
2021-03-18 05:40:16 -0500

    added more advanced schema logic

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 const {ApolloServer} = require("apollo-server");
 
+//=====Dummy Data =====//
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+
+// The exclaimation point means that the info field is required. Its like notNullable in sql
 const typeDefs = `
 type Query {
     info: String!
+    feed : [Link!]!
+}
+
+type Link {
+    id: ID!
+    description : String!
+    url: String!
 }
 `
-
+// The object here is the actual implementation of the GraphQL schema.
 const resolvers = {
     Query : {
-        info: () => `This is the api for a hackernews clone tutorial`
+        info: () => `Welcome to graphql brother`,
+        feed: () => links,
+    },
+    Link : {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
     }
 }
 
